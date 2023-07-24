dab715c0dc2d5f60dc4950d3114b82ffdb7bdc42
Caroline Calixto <79118657+Caroline-Calixto@users.noreply.github.com>
2023-07-24 17:31:38 -0300

    Inclusão de banco de dados

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const app = express();
 
+const bodyParser = require('body-parser');
+
+const connection = require("./database/database");
+const perguntaModel = require("./database/Pergunta");
+
+//Database
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão feita com o banco de dados!")
+    })
+    .catch((msgErro) => {
+        console.log(msgErro);
+    })
+
 app.set("view engine", "ejs"); // indicando pra o express usar o EJS como view engine
 app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: false})); // decodificar os dados enviados pelo forms
+app.use(bodyParser.json()); // config para usar o formato json
 
 app.get("/", (req, resp) => {
     resp.render("home");
@@ -10,6 +28,12 @@ app.get("/", (req, resp) => {
 
 app.get("/perguntar", (req, resp) => {
     resp.render("perguntar");
+});
+
+app.post("/salvarpergunta",(req, res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("Formulário recebido! titulo " + titulo + " " + " descricao " + descricao);
 });
 
 
